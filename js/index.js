5d47ca6ab03fa563c81ebe73678534b41027abd6
Ariel Pontes <pontes.ariel@gmail.com>
2016-07-28 19:17:21 +0300

    Isolate some functions to make code more modular/reusable.

--- js/index.js
+++ js/index.js
@@ -1,46 +1,68 @@
-$(function() {
+function loadSettings(){
+  SETTINGS = {
+    "medium": $("#medium")[0].checked,
+    "parenthesis": $("#parenthesis")[0].checked
+  };
+}
 
-  function loadSettings(){
-    SETTINGS = {
-      "medium": $("#medium")[0].checked,
-      "parenthesis": $("#parenthesis")[0].checked
-    };
+function numToSup(num){
+  var supChars = "⁰¹²³⁴⁵⁶⁷⁸⁹";
+  var str = num.toString();
+  var sup = str.replace(
+    /0|1|2|3|4|5|6|7|8|9/gi, function myFunction(x){return supChars[x];});
+  if(SETTINGS["parenthesis"])
+    return "⁽"+sup+"⁾";
+  return " "+sup;
+}
+
+function refLink(refNum){
+  if(SETTINGS["medium"]){
+    var supRef = numToSup(refNum);
+    return '<a href="#fn'+refNum+'">'+supRef+'</a>';
+  } else {
+    return '<sup><a href="#fn'+refNum+'" id="fna'+refNum
+      +'" style="'+A_STYLE+'">['+refNum+']</a></sup>';
   }
+}
 
-  function numToSup(num){
-    var supChars = "⁰¹²³⁴⁵⁶⁷⁸⁹";
-    var str = num.toString();
-    var sup = str.replace(
-      /0|1|2|3|4|5|6|7|8|9/gi, function myFunction(x){return supChars[x];});
-    if(SETTINGS["parenthesis"])
-      return "⁽"+sup+"⁾";
-    return " "+sup;
-  }
-
-  function refLink(refNum){
+function getRefList(references, ref_p_names){
+  var refList = $('<ol></ol>');
+  $(references).each(function(i, ref){
+    var ref_li;
     if(SETTINGS["medium"]){
-      var supRef = numToSup(refNum);
-      return '<a href="#fn'+refNum+'">'+supRef+'</a>';
+      ref_li = '<li name="fn'+(i+1)+'"><a href="#'+ ref_p_names[i] +'">^ </a> '+ref.innerHTML+'</li>';
     } else {
-      return '<sup><a href="#fn'+refNum+'" id="fna'+refNum
-        +'" style="'+A_STYLE+'">['+refNum+']</a></sup>';
+      ref_li = '<li id="fn'+(i+1)+'"><a href="#fna'+(i+1)
+      +'" style="'+A_STYLE+'">^</a> '+ref.innerHTML+'</li>';
+    }
+    refList.append(ref_li);
+  });
+  return refList;
+}
+
+function setDefaultStyles(){
+  A_STYLE = "text-decoration: none;"; // Default style to be used for the
+  // superscript links that will be generated
+  var aFirst = $('a').first(); // Get the first link
+  if(aFirst){
+    var aFirstStyle = aFirst.attr('style'); // If it has a style, use it as
+    // default style for new links instead
+    if(aFirstStyle){
+      A_STYLE = aFirstStyle;
     }
   }
-
-  function getRefList(references, ref_p_names){
-    var refList = $('<ol style="'+P_STYLE+'"></ol>');
-    $(references).each(function(i, ref){
-      var ref_li;
-      if(SETTINGS["medium"]){
-        ref_li = '<li name="fn'+(i+1)+'"><a href="#'+ ref_p_names[i] +'">^ </a> '+ref.innerHTML+'</li>';
-      } else {
-        ref_li = '<li id="fn'+(i+1)+'"><a href="#fna'+(i+1)
-        +'" style="'+A_STYLE+'">^</a> '+ref.innerHTML+'</li>';
-      }
-      refList.append(ref_li);
-    });
-    return refList;
+  // Same for h1...
+  H1_STYLE = "";
+  var firstH1 = $('h1').first();
+  if(firstH1){
+    var styleAttr = firstH1.attr('style');
+    if(styleAttr){
+      H1_STYLE = styleAttr;
+    }
   }
+}
+
+$(function() {
 
   var btn = $('#button'); // The button that triggers the conversion
 
@@ -66,34 +88,8 @@ $(function() {
     // A list to hold all references we find
     var ref_p_names = []
     // A list to hold the names of the parent paragraph of each reference
-
-    A_STYLE = "text-decoration: none;"; // Default style to be used for the
-    // superscript links that will be generated
-    var aFirst = $('a').first(); // Get the first link
-    if(aFirst){
-      var aFirstStyle = aFirst.attr('style'); // If it has a style, use it as
-      // default style for new links instead
-      if(aFirstStyle){
-        A_STYLE = aFirstStyle;
-      }
-    }
-    // Same for paragraphs...
-    P_STYLE = "";
-    var pFirst = $('p').first();
-    if(pFirst){
-      var pFirstStyle = pFirst.attr('style');
-      if(pFirstStyle){
-        P_STYLE = pFirstStyle;
-      }
-    }
-    // Same for h1...
-    H1_STYLE = "";
-    var firstH1 = $('h1').first();
-    if(firstH1){
-      var styleAttr = firstH1.attr('style');
-      if(styleAttr){
-        H1_STYLE = styleAttr;
-      }
+    if(!SETTINGS["medium"]){
+      setDefaultStyles();
     }
     textDiv.children().each(function(i, elem){
       // For each child element (usually <p>) in the text body
